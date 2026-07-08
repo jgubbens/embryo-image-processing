e6873bf6ea9297af84898142c8bbc26da94299f7
jgubbens <146300159+jgubbens@users.noreply.github.com>
2026-07-08 09:57:01 -0400

    PyCLM experiment

--- PyCLM/pyclm_classifier.py
+++ PyCLM/pyclm_classifier.py
@@ -43,4 +43,3 @@ if __name__ == "__main__":
     pattern_methods = {"classify_embryos": ClassifyEmbryos}
 
     run_pyclm(BASE_PATH, pattern_methods=pattern_methods, position_mover=PFSPositionMover(), gui=True)
-    
